de212dec3ae0f2cbf147907b28a5fd04fc589984
Faculdade Embu das Artes <90737370+JeanPDR@users.noreply.github.com>
2024-10-17 20:03:49 -0300

    definir os tipos para as props

--- src/components/ComponenteModular.tsx
+++ src/components/ComponenteModular.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+//Definir os Tipos para as Props
+type Card = {
+  titulo: string;
+  paragrafo: string;
+  videos: number;
+  progresso: number;
+};
+
 function ComponenteModular() {
   return (
     <>
