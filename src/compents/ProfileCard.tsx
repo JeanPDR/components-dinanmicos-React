1621ed195cf4ce1529e9f195460b3133ccea1b8b
Faculdade Embu das Artes <90737370+JeanPDR@users.noreply.github.com>
2024-10-17 11:06:43 -0300

    criando compoenente statico

---
+++ src/compents/ProfileCard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ProfileCard() {
+  return (
+    <div>
+      <h1>Profile Card</h1>
+      <h2>John Doe</h2>
+      <p> Software Engineer</p>
+    </div>
+  );
+}
+
+export default ProfileCard;
